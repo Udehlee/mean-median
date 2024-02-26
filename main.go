64aeb030237c038b5388a1929d68a0801050d597
udehlee <ritzeepita@gmail.com>
2024-02-26 14:00:09 +0100

    Optimize mean and median calculations for improved benchmark performance

--- main.go
+++ main.go
@@ -6,49 +6,44 @@ import (
 	"sort"
 )
 
+// Mean returns the average of the nums
 func Mean(nums []float64) (float64, error) {
 
-	lengthOfNums := len(nums)
-
-	if lengthOfNums == 0 {
+	if len(nums) == 0 {
 		return 0, errors.New("no number(s) inputted")
 	}
-	var sum float64
-	// sum = 0
 
+	sum := 0.0
 	for _, v := range nums {
 		sum += v
 	}
-
-	return sum / float64(lengthOfNums), nil
+	return sum / float64(len(nums)), nil
 
 }
 
+// Median returns the middle number
 func Median(nums []float64) (float64, error) {
 
-	lengthOfNums := len(nums)
-
-	if lengthOfNums == 0 {
+	if len(nums) == 0 {
 		return 0, errors.New("no number(s) inputted")
 	}
 	//sort the numbers in an ascending order
 	sort.Float64s(nums)
 
-	if lengthOfNums%2 == 1 {
+	if len(nums)%2 == 1 {
 		//if its odd, return the middle number
-		return float64(nums[lengthOfNums/2]), nil
+		return float64(nums[len(nums)/2]), nil
 	} else {
 		//if its even return, add the two middle numbers and divide by two
-		return float64(nums[lengthOfNums/2-1]+nums[lengthOfNums/2]) / 2, nil
+		return float64(nums[len(nums)/2-1]+nums[len(nums)/2]) / 2, nil
 	}
 }
+
 func main() {
-	xi := []float64{1, 2, 3, 4, 5, 1}
+	nums := []float64{2, 2, 3, 4, 5}
 
-	//mean
-	fmt.Println(Mean(xi))
+	fmt.Println(Mean(nums))
 
-	//median
-	fmt.Println(Median(xi))
+	fmt.Println(Median(nums))
 
 }
